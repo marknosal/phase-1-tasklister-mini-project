954d7ebb260cd833e94bf109995eb6449f3d99a9
Mark Nosal <mark.c.nosal@gmail.com>
2023-01-28 14:50:43 -0600

    Finished lab, can delete tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault()
+    inputHandler(document.getElementById('new-task-description').value)
+    document.querySelector('form').reset()
+  })
+
 });
+
+function inputHandler (input) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', () => deleteHandler(p))
+  btn.textContent = ' X'
+  p.textContent = `${input} `
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+  
+}
+
+function deleteHandler (p) {
+  p.remove()
+}
